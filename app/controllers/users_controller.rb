43b4487bcdd496e714da9b7e3d1e7950a2fbb3e7
Monika Filipčiková <xfilipcikovam@stuba.sk>
2015-05-15 09:58:11 +0200

    oprava chyby

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,7 @@ before_action :correct_user, only: [:edit, :update]
 before_action :admin_user,  only: :destroy
 
 
-   def my_parking_places
-     @mypark= current_user.
-   end
+ 
    def show
      @parking_plac = current_user.parking_places.build if logged_in?
     #1.@user = User.find_by_sql("SELECT * FROM users WHERE users.id = #{params[:id]}").first
